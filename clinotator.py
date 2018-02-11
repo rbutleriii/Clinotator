87ec3bebf7e9d2692a3fc1356f221ee272739d72
rbutleriii <rbutler1@hawk.iit.edu>
2018-02-10 20:56:51 -0600

    input handling function

--- clinotator.py
+++ clinotator.py
@@ -20,7 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import argparse, re, logging, sys
 import pandas as pd
-import Bio.Entrez as Entz
+from Bio import Entrez
 
 __version__ = "0.1.0"
 
@@ -49,25 +49,67 @@ def getargs():
                                help='NCBI requires an email for querying their databases')
     return parser.parse_args()
 
+def input_selection(type, file, vid_list):
+    try:
+        if type == 'vid':
+            with open(file) as f:
+                vid_list = [line.rstrip('\n') for line in f]
+            return vid_list
+        
+        elif type == 'rsid':
+            with open(file) as f:
+                rsid_list = [line.rstrip('\n') for line in f]
+                rsid_to_vid(rsid_list, vid_list)
+            return vid_list
+        
+        elif type == 'vcf':
+            with open(file) as f:
+                print('vcf parsing is not yet implemented, coming soon...')
+                sys.exit()
+                # implement vcf object? or generate list
+
+    except IOError as e:
+        errno, strerror = e.args
+        logging.fatal(e)
+        print('Unable to open file, Error {}: {}'.format(errno,strerror))
 
 # looking up rsids and converting them to vids
-def rsid_to_vid(rsid_list):
-    # import Bio.Entrez as Entz
-    # Entz.email = args.email
-    # Entz.tool = 'Clinotator' # preferred by NCBI
+def rsid_to_vid(rsid_list, vid_list):
+    # import Bio.Entrez as Entrez
+    # Entrez.email = args.email
+    # Entrez.tool = 'Clinotator' # preferred by NCBI
 
-    results = Entz.read(Entz.epost('snp' , id = ','.join(rsid_list)))
-    webenv = results['WebEnv']
-    query_key = results['QueryKey']
+    logging.debug('rsid_list {}'.format(rsid_list))
+    query = Entrez.read(Entrez.epost('snp', id=",".join(rsid_list)))
+    webenv = query['WebEnv']
+    query_key = query['QueryKey']
+    logging.debug('returned webenv: {} returned query key {}'.format(webenv, query_key))
+    query_elink = Entrez.read(Entrez.elink(dbfrom='snp', db='clinvar', webenv=webenv, query_key=query_key)) # REPLACE WITH WEB HISTORY
+    for link_list in query_elink:
+        for linksetdb in link_list['LinkSetDb']:
+            for link_id in linksetdb['Link']:
+                vid_list.append(link_id['Id'])
+                # each link query returns an 'IdList' of one, so always link_list['IdList'][0].
+                # May not work with webenv, as join instead returns one search result for all
+                logging.debug('rsid {} returns VID: {}'.format(link_list['IdList'][0], link_id['Id']))
+    logging.debug('vid_list -> {}'.format(vid_list))
     
-    return
+    return vid_list
     
 def main():
+    Entrez.tool = 'Clinotator' # preferred by NCBI
     logging.basicConfig(level=logging.DEBUG)
+    
     args = getargs()
+    Entrez.email = args.email
     logging.debug('CLI inputs are {} {} {}'.format(args.type, args.email, args.input))
-    Entz.tool = 'Clinotator' # preferred by NCBI
-    Entz.email = args.email
+    
+    for file in args.input:
+        vid_list = []
+        input_selection(args.type, file, vid_list)
+        logging.debug('vid_list to query clinvar: total {} items'.format(len(vid_list)))
+    
+    
 
 if __name__ == '__main__':
     main()
